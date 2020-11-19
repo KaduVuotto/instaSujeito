5b8e7a036649270148b1a31c3937c2941eadf5e8
CarlosAmorim <carlos.amorim@rckyinformatica.onmicrosoft.com>
2020-11-19 11:36:28 -0300

    primeiro commit

---
+++ src/lista/index.js
@@ -0,0 +1,160 @@
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    Image,
+    StyleSheet,
+} from 'react-native';
+
+class Lista extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            feed: this.props.data
+        };
+
+        this.mostraLikes = this.mostraLikes.bind(this);
+        this.like = this.like.bind(this);
+        this.carregaIcone = this.carregaIcone.bind(this);
+    }
+
+    carregaIcone(likeada) {
+        return likeada ?
+            require('../img/likeada.png')
+            : require('../img/like.png')
+    }
+
+    like() {
+        let feed = this.state.feed
+
+        if (feed.likeada) {
+            this.setState({
+                feed: {
+                    ...feed,
+                    likeada: false,
+                    likers: feed.likers - 1
+                }
+            })
+        } else {
+            this.setState({
+                feed: {
+                    ...feed,
+                    likeada: true,
+                    likers: feed.likers + 1
+                }
+            })
+        }
+    }
+
+    mostraLikes(likers) {
+        let feed = this.state.feed;
+
+        if (feed.likers <= 0) {
+            return;
+        }
+
+        return (
+            <Text style={styles.likes}>
+                {feed.likers} {feed.likers > 1 ? 'curtidas' : 'curtida'}
+            </Text>
+        )
+    }
+
+    render() {
+        return (
+            <View style={styles.areaFeed}>
+
+                <View style={styles.areaPerfil}>
+                    <Image source={{ uri: this.state.feed.imgperfil }} style={styles.fotoPerfil} />
+                    <Text style={styles.nomeUsuario}> {this.state.feed.nome} </Text>
+                </View>
+
+                <Image resizeMode='cover' source={{ uri: this.state.feed.imgPublicacao }} style={styles.fotoPublicacao} />
+
+                <View style={styles.areaBtn}>
+                    <TouchableOpacity onPress={this.like}>
+                        <Image source={this.carregaIcone(this.state.feed.likeada)} style={styles.iconeLike} />
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.btnSend}>
+                        <Image source={require('../img/send.png')} style={styles.iconeLike} />
+                    </TouchableOpacity>
+                </View>
+
+                {this.mostraLikes(this.state.feed.likers)}
+
+                <View style={styles.viewRodape}>
+                    <Text style={styles.nomeRodape}>
+                        {this.state.feed.nome}
+                    </Text>
+
+                    <Text style={styles.descRodape}>
+                        {this.state.feed.descricao}
+                    </Text>
+                </View>
+
+
+
+
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    areaFeed: {},
+    nomeUsuario: {
+        fontSize: 22,
+        textAlign: 'left',
+        color: 'black',
+    },
+    fotoPerfil: {
+        width: 50,
+        height: 50,
+        borderRadius: 25
+    },
+    fotoPublicacao: {
+        flex: 1,
+        height: 400,
+        alignItems: 'center',
+    },
+    areaPerfil: {
+        flexDirection: 'row',
+        flex: 1,
+        alignItems: 'center',
+        padding: 8
+    },
+    areaBtn: {
+        flexDirection: 'row',
+        padding: 5,
+    },
+    iconeLike: {
+        height: 33,
+        width: 33,
+    },
+    btnSend: {
+        paddingLeft: 5,
+    },
+    viewRodape: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    descRodape: {
+        paddingLeft: 5,
+        fontSize: 15,
+        color: 'black',
+    },
+    nomeRodape: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: 'black',
+        paddingLeft: 5,
+    },
+    likes: {
+        fontWeight: 'bold',
+        marginLeft: 5
+    },
+})
+
+export default Lista;
